b4001fedc408706f27d0901329bda8ccbf887f23
hhmaho <42540357+hhmaho@users.noreply.github.com>
2018-10-22 17:17:02 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// let firstInputNumber = 5
-// let secondInputNumber = 2
-// function plusOperator() {
-//     return firstInputNumber + secondInputNumber
-// }
-// console.log(plusOperator())
-
-// const plusOperator = (props) => {
-//   return 5 + 2 //later props.digit = input(called props)
-// }
-// console.log(plusOperator())
-
-// function Digit(props) {
-//   return (
-//     <div className="Digit" >
-//       <button>1</button>
-//       <button>2</button>
-//       <button>3</button>
-//     </div>
-//   )
-// }
-
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-// const listDigits = numbers.map(number => <button key={"digit_" + number}>{number}</button>);
-
-
 function Operators(props) {
   return (
     <div className="Operator">
@@ -51,8 +25,6 @@ class App extends Component {
     this.state = {
       input: "1 + 1",
       output: "test",
-      // listDigits,
-      // Returnresult: 0
     };
     console.log(this.state)
   }
@@ -62,8 +34,6 @@ class App extends Component {
       <div className="App">
         <span>{this.state.input}</span>
         <br />
-        {/* <span>{this.state.listDigits}</span>
-        <br /> */}
         <button onClick={() => this.setState({ input: this.state.input.concat("1") })}>1</button>
         <button onClick={() => this.setState({ input: this.state.input.concat("2") })}>2</button>
         <button onClick={() => this.setState({ input: this.state.input.concat("3") })}>3</button>
@@ -71,24 +41,14 @@ class App extends Component {
         <br />
         <span>{this.state.output}</span>
         <button onClick={() => this.setState({ output: this.state.input })}>=</button>
-        {/* <button onClick={() => console.log("abc")}>click</button> */}
-        {/* <Digit onClick={this.props}></Digit> */}
         <Operators onClick={this.props}></Operators>
         <Equal
           onClick={this.props}>
         </Equal>
-        {/* <input>{this.calculResult()}</input> */}
       </div >
     );
 
   }
-  // //method
-  // calculResult() {
-  //   //const {result} = this.state;//object destructuring ipv this.state
-  //   return (
-  //     "1 + 1"
-  //   ) //result//hier eventueel foutmelding: this.state.count === "string" ? <p>input error</p> : this.state.count;
-  // }
 }
 
 export default App;
